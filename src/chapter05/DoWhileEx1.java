9c7b70940341671bfaf0be3b625f6fd176ec4a30
kimyeju <kimyj9508@gmail.com>
2024-01-03 21:50:15 +0900

    java study - chapter05 (제어문 - 반복문 : do~while문)

---
+++ src/chapter05/DoWhileEx1.java
@@ -0,0 +1,17 @@
+package chapter05;
+
+public class DoWhileEx1 {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		
+		do {
+			System.out.println("i = "+i);
+		} while (i < 10);
+
+	}
+
+	// 조건식이 밑에 있기 때문에 조건식 확인 전에 출력을 먼저하고 조건식의 결과 false이르모 반복되지 않고 종료된 것이다.
+	
+}
