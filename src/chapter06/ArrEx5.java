dbbc278b7499f96b9160a7d87d7f9821f2e31928
kimyeju <kimyj9508@gmail.com>
2024-01-08 23:50:06 +0900

    java study - chapter06 (배열 - 배열 선언)

---
+++ src/chapter06/ArrEx5.java
@@ -0,0 +1,18 @@
+package chapter06;
+
+public class ArrEx5 {
+
+	public static void main(String[] args) {
+		
+		// 배열 변수를 선언과 동시에 초기화 하는 방법
+		String[] arrStr = {"홍길동", "이순신", "김유신"};
+		
+		System.out.println(arrStr[0]);
+		System.out.println(arrStr[1]);
+		System.out.println(arrStr[2]);
+		
+		// 배열을 선언하는 방법은 여러가지 방법이 있지만, 주의할 점은 처음 배열을 선언할 때 배열의 길이가 정해지며, 나중에 길이를 변경할 수 없다.
+
+	}
+
+}
