22a131fcb67a250f060c254978ebb70e174d27d1
kimyeju <kimyj9508@gmail.com>
2024-01-08 23:46:50 +0900

    java study - chapter06 (배열 - 배열 선언)

---
+++ src/chapter06/ArrEx3.java
@@ -0,0 +1,20 @@
+package chapter06;
+
+public class ArrEx3 {
+
+	public static void main(String[] args) {
+		
+		int[] arrInt = new int[5]; // 길이가 5개인 배열 객체 생성
+		
+		arrInt[0] = 1;
+		arrInt[1] = 2;
+		arrInt[2] = 3;
+		arrInt[3] = 4;
+		arrInt[4] = 5;
+		
+		System.out.println(arrInt[0]);
+		System.out.println(arrInt[1]);
+
+	}
+
+}
--- src/chapter06/ArrEx4.java
+++ src/chapter06/ArrEx4.java
@@ -7,6 +7,10 @@ public class ArrEx4 {
 		String[] arrStr = new String[5];
 		
 		System.out.println(arrStr[0]);
+		
+		// String str = " ";
+		// String str = null;
+		// 위 두 코드는 완전히 다르다. ""는 빈문자열이 있는 것이고, null은 값 자체가 존재하지 않는 것이므로 혼동하지 않도록 하자.
 
 	}
 
