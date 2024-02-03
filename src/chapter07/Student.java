a91482cc709d44a45def48f150bf9fe82400e4cd
kimyeju <kimyj9508@gmail.com>
2024-02-03 23:16:35 +0900

    java study - chapter07 (클래스 - 생성자 : 변수 초기화)

---
+++ src/chapter07/Student.java
@@ -0,0 +1,16 @@
+package chapter07;
+
+public class Student {
+
+	// 필드
+	String name; // 학생명
+	int grade; // 학년
+	String department; // 학과
+	
+	Student(String n, int g, String d) {
+		name = n;
+		grade = g;
+		department = d;
+	}
+	
+}
