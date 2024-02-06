3cf669ac94a83b146720fad6b81f4b9b0af3d778
kimyeju <kimyj9508@gmail.com>
2024-02-06 22:52:35 +0900

    java study - chapter07 (클래스 - 생성자 : 생성자 오버로딩)

--- src/chapter07/Student.java
+++ src/chapter07/Student.java
@@ -7,15 +7,42 @@ public class Student {
 	int grade; // 학년
 	String department; // 학과
 	
+	// 변수 초기화 부분
+	
+//	Student(String n, int g, String d) {
+//		name = n;
+//		grade = g;
+//		department = d;
+//	}
+//	
+//	// 기본 생성자
+//	Student() {
+//		
+//	}
+	
+	// 생성자 오버로딩 부분
+
+	// 1번 생성자
+	Student() {
+		
+	}
+	
+	// 2번 생성자
+	Student(String n) {
+		name = n;
+	}
+	
+	//3번 생성자
+	Student(String n, int g) {
+		name = n;
+		grade = g;
+	}
+	
+	// 4번 생성자
 	Student(String n, int g, String d) {
 		name = n;
 		grade = g;
 		department = d;
-	}
-	
-	// 기본 생성자
-	Student() {
-		
 	}
 	
 }
