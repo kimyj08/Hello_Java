70f414be9ff9a787e46c435d34917b6385b4dd43
kimyeju <kimyj9508@gmail.com>
2024-02-21 23:15:20 +0900

    java study - chapter08 (상속 - 클래스 상속 : super())

---
+++ src/chapter08/SuperEx2.java
@@ -0,0 +1,19 @@
+package chapter08;
+
+public class SuperEx2 {
+
+}
+
+class Parent2 {
+	
+	String name;
+	
+	Parent2(String name) {
+		this.name = name;
+	}
+	
+}
+
+class Child extends Parent2 { // 에러 발생
+	
+}
