68ce8dba51576a580c59d06d33c77cd2c453a895
kimyeju <kimyj9508@gmail.com>
2024-03-04 22:43:46 +0900

    java study - chapter08 (상속 - 상속관계에서 접근제한자)

---
+++ src/chapter08/pkg1/Aclass.java
@@ -0,0 +1,16 @@
+package chapter08.pkg1;
+
+public class Aclass {
+
+	protected String vatA;
+	String varA2;
+	
+	protected void methodA() {
+		System.out.println("methodA");
+	}
+	
+	void methodA2() {
+		System.out.println("methodA2");
+	}
+	
+}
