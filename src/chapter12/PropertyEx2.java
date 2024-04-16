59164641298cdd8f28f7541943dad35acec04c3b
kimyeju <kimyj9508@gmail.com>
2024-04-16 22:22:11 +0900

    java study - chapter12 (기본 API - java.lang 패키지[System 클래스])

---
+++ src/chapter12/PropertyEx2.java
@@ -0,0 +1,13 @@
+package chapter12;
+
+public class PropertyEx2 {
+
+	public static void main(String[] args) {
+		
+		for (String var : System.getenv().keySet()) {
+			System.out.println(var + "=" + System.getenv(var));
+		}
+
+	}
+
+}
