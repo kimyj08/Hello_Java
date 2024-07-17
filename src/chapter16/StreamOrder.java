925a18dcdb2adab5bb85a6419a6162b07368ab29
kimyeju <kimyj9508@gmail.com>
2024-07-17 23:45:27 +0900

    java study - chapter16 (스트림 - 스트림 가공하기:정렬(Sorting))

--- src/chapter16/StreamOrder.java
+++ src/chapter16/StreamOrder.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public class StreamOther2 {
+public class StreamOrder {
 
 	public static void main(String[] args) {
 		
